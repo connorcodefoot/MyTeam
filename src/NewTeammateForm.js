b2b85cbe677b3e7738c0ad0397bcc179544edba6
Connor Broadfoot <cgbroadfoot@gmail.com>
2023-08-01 15:44:07 -0700

    Update styles for teammate form

--- src/NewTeammateForm.js
+++ src/NewTeammateForm.js
@@ -31,21 +31,21 @@ function NewTeammateForm (props) {
 
   return(
     <>
-  <form>
-    <label for="name">Name:</label>
+  <form id="new-teammate-form">
+    <label for="name">Teammate Name</label>
     <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}></input>
 
-    <label for="title">Title:</label>
+    <label for="title">Job Title</label>
     <input type="text" id="title" name="title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
 
-    <label for="verbose">Verbose:</label>
+    <label for="verbose">Verbosity</label>
     <input type="range" id="verbose" name="verbose" min="0" max="5" step="1" value={verbose} onChange={(e) => setVerbose(e.target.value)}></input>
 
-    <label for="temperature">Temperature:</label>
+    <label for="temperature">Creativity</label>
     <input type="range" id="temperature" name="temperature" min="0" max="5" step="1" value={temperature} onChange={(e) => setTemperature(e.target.value)}></input>
 
-    <label for="character">Persona Description:</label>
-    <textarea id="character" name="character" value={character} onChange={(e) => setCharacter(e.target.value)}></textarea>
+    <label for="character">Persona Description</label>
+    <textarea id="character" name="character" rows="3" value={character} onChange={(e) => setCharacter(e.target.value)}></textarea>
 
     <button type="submit" value="Submit" onClick={handleSubmit}>Save</button>
   </form>
